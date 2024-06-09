ef7a0046d962caa185e5ef3b8ffb9e7a1c7d25ac
RasmusAC <ralc@itu.dk>
2024-06-09 20:57:26 +0200

    introduction text

--- src/App.jsx
+++ src/App.jsx
@@ -36,17 +36,12 @@ function App() {
             <div style={{ width: '100%' }}><h1>RaChat</h1></div>
           </header>
         }
-        {/* <header>
-          <div></div>
-          <div><h1>RaChat</h1></div>
-          <div><SignOut auth={auth} /></div>
-        </header> */}
         <section>
           {user ? <ChatRoom messagesRef={messagesRef} auth={auth} /> :
             <>
               <div className="welcome-message">
                 <h2>Welcome to RaChat</h2>
-                <p>This is my own home-made live chat platform. To be able to use the chat, you'll have to sign in using a Google account. Feel free to try it out, but remember to keep a sober tone, since it's a public chat. To delete a message of your own, you can click on your profile picture besides the message.</p>
+                <p>This is my own home-made live chat platform. To be able to use the chat, you'll have to sign in using a Google account. Feel free to try it out, but remember to keep a sober tone, since it's a public chat. To delete a message of your own, you can click on your profile picture besides the message. Visit the <a style={{ color: '#4385f0' }} href="https://github.com/RasmusAChr/RaChat">Github Repository</a> to learn more.</p>
               </div>
               <SignIn auth={auth} />
             </>}
